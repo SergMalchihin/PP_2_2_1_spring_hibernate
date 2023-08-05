21cf9cbb2c6aa4e16664f1a9c737d1f002da589c
Sergey Malchihin <s.malchihin@gmail.com>
2023-08-05 13:27:39 +0300

    PP_2_2_1_spring_hibernate Отправлен один запрос для получения владельца машины по модели и серии к таблице Юзерс, без использования цикла и отдельные запросы

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,23 +16,23 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
 
-      CarService carService = context.getBean(CarService.class);
+//      CarService carService = context.getBean(CarService.class);
 //      carService.deleteAllCars();
-      carService.add(new Car("MB", "S500"));
-      carService.add(new Car("MB", "E300"));
-      carService.add(new Car("BMW", "X5"));
-      carService.add(new Car("BMW", "520i"));
+//      carService.add(new Car("MB", "S500"));
+//      carService.add(new Car("MB", "E300"));
+//      carService.add(new Car("BMW", "X5"));
+//      carService.add(new Car("BMW", "520i"));
 
 
       UserService userService = context.getBean(UserService.class);
 //      userService.deleteAllUsers();
-      List<Car> cars = carService.listCars();
-
-
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", cars.get(0)));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", cars.get(1)));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru", cars.get(2)));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru", cars.get(3)));
+//      List<Car> cars = carService.listCars();
+//
+//
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru", cars.get(0)));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru", cars.get(1)));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru", cars.get(2)));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru", cars.get(3)));
 
       System.out.println("Владелец \"MB E300\"");
       System.out.println(userService.carOwner("MB", "E300"));
